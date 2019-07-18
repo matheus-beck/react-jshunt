891fdff63b5aa81874afe26d52c38beac4128408
matheus-beck <matheus.alencarbeck@gmail.com>
2019-07-18 20:15:33 -0300

    Updates

---
+++ precache-manifest.eb6db4f190853ecd98ad966322497ed5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "12768305806ed777989f",
+    "url": "/react-jshunt/static/js/runtime~main.12768305.js"
+  },
+  {
+    "revision": "0c3ca82351a47b3e4879",
+    "url": "/react-jshunt/static/js/main.0c3ca823.chunk.js"
+  },
+  {
+    "revision": "793665c0aeb95aa09771",
+    "url": "/react-jshunt/static/js/1.793665c0.chunk.js"
+  },
+  {
+    "revision": "0c3ca82351a47b3e4879",
+    "url": "/react-jshunt/static/css/main.bf1dca9d.chunk.css"
+  },
+  {
+    "revision": "d986aa91c37a0fdf61805951695ace05",
+    "url": "/react-jshunt/index.html"
+  }
+];
